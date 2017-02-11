701c6f28725e750428698303c99dbcd88a97b1b5
Katie <kaspii@users.noreply.github.com>
2017-02-11 10:43:37 -0800

    Added basic map to index

---
+++ main.js
@@ -0,0 +1,6 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2FzcGkiLCJhIjoiY2l6MWt6czNuMDVnMTJxbjEycnB1Nmt3MiJ9.25IwYCMCXhrhtgKBsFfdNA';
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9'
+});

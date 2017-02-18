9d132dedeb975cd7ee98006bf869a6286ad5a94a
Benson Han <bensonjaminhan@gmail.com>
2017-02-18 14:30:40 -0800

    article tabs yay

--- main.js
+++ main.js
@@ -5,26 +5,4 @@ var map = new mapboxgl.Map({
     zoom: 13,
     center: [-118.43791, 34.05048],
     style: 'mapbox://styles/mapbox/streets-v9'
-});
-
-//smooth scrolling
-$('a[href*="#"]:not([href="#"])').click(function() {
-  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-    if (target.length) {
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000);
-      return false;
-    }
-  }
-});
-
-$('.menu li a').click(function(e) {
-  var $this = $(this);
-  if (!$this.hasClass('active')) {
-    $this.addClass('active');
-  }
-  e.preventDefault();
-});
+})

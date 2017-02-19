06db8d08fff685bb00da507df1411b9b29fed436
Jiunn (Leo) Siow <siowleo2@gmail.com>
2017-02-18 16:12:08 -0800

    Update main.js
    added markers

--- main.js
+++ main.js
@@ -6,3 +6,45 @@ var map = new mapboxgl.Map({
     center: [-118.43791, 34.05048],
     style: 'mapbox://styles/mapbox/streets-v9'
 })
+
+map.on('style.load', function (e) {
+    console.log(e.style.sprite);
+    map.addSource('markers', {
+        "type": "geojson",
+        "data": {
+            "type": "FeatureCollection",
+            "features": [{
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [34.050646, -118.437671]
+                },
+                "properties": {
+                    "title": "Ramyani Westwood",
+                    "marker-symbol": "default_marker"
+                }
+            }, {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [34.047748, -118.435237]
+                },
+                "properties": {
+                    "title": "Taste of Tehran",
+                    "marker-color": "#ff00ff",
+                    "marker-symbol": "secondary_marker"
+                }
+            }]
+        }
+    });
+
+    map.addLayer({
+        "id": "markers",
+        "source": "markers",
+        "type": "circle",
+        "paint": {
+            "circle-radius": 10,
+            "circle-color": "#007cbf"
+        }
+    });
+});

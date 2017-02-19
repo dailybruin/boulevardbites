fdae979e04ffd3037493c03f529917fad2212bc7
Leo Siow <siowleo2@gmail.com>
2017-02-18 22:41:23 -0800

    json changes

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var map = new mapboxgl.Map({
   // style URL
   style: 'mapbox://styles/mapbox/light-v9',
   // initial position in [long, lat] format
-  center: [-77.034084, 38.909671],				// NOTE: Change this to [-118.43791, 34.05048] once we add in our locs
+  center: [-118.43791, 34.05048],				// NOTE: Change this to [-118.43791, 34.05048] once we add in our locs
   // initial zoom
   zoom: 14
 });
@@ -18,8 +18,8 @@ var stores =       {
             "geometry": {
               "type": "Point",
               "coordinates": [
-                -77.034084142948,
-                38.909671288923
+                -118.437929,
+                34.050762
               ]
             },
             "properties": {
@@ -38,8 +38,8 @@ var stores =       {
             "geometry": {
               "type": "Point",
               "coordinates": [
-                -77.049766,
-                38.900772
+                -118.443339,
+                34.048308
               ]
             },
             "properties": {
@@ -58,8 +58,8 @@ var stores =       {
             "geometry": {
               "type": "Point",
               "coordinates": [
-                -77.043929,
-                38.910525
+                -118.452479,
+                34.021056
               ]
             },
             "properties": {
@@ -78,8 +78,8 @@ var stores =       {
             "geometry": {
               "type": "Point",
               "coordinates": [
-                -77.0672,
-                38.90516896
+                -118.435216,
+                34.047775
               ]
             },
             "properties": {
@@ -93,161 +93,7 @@ var stores =       {
               "state": "D.C."
             }
           },
-          {
-            "type": "Feature",
-            "geometry": {
-              "type": "Point",
-              "coordinates": [
-                -77.002583742142,
-                38.887041080933
-              ]
-            },
-            "properties": {
-              "phoneFormatted": "(202) 547-9338",
-              "phone": "2025479338",
-              "address": "221 Pennsylvania Ave SE",
-              "city": "Washington DC",
-              "country": "United States",
-              "crossStreet": "btwn 2nd & 3rd Sts. SE",
-              "postalCode": "20003",
-              "state": "D.C."
-            }
-          },
-          {
-            "type": "Feature",
-            "geometry": {
-              "type": "Point",
-              "coordinates": [
-                -76.933492720127,
-                38.99225245786
-              ]
-            },
-            "properties": {
-              "address": "8204 Baltimore Ave",
-              "city": "College Park",
-              "country": "United States",
-              "postalCode": "20740",
-              "state": "MD"
-            }
-          },
-          {
-            "type": "Feature",
-            "geometry": {
-              "type": "Point",
-              "coordinates": [
-                -77.097083330154,
-                38.980979
-              ]
-            },
-            "properties": {
-              "phoneFormatted": "(301) 654-7336",
-              "phone": "3016547336",
-              "address": "4831 Bethesda Ave",
-              "cc": "US",
-              "city": "Bethesda",
-              "country": "United States",
-              "postalCode": "20814",
-              "state": "MD"
-            }
-          },
-          {
-            "type": "Feature",
-            "geometry": {
-              "type": "Point",
-              "coordinates": [
-                -77.359425054188,
-                38.958058116661
-              ]
-            },
-            "properties": {
-              "phoneFormatted": "(571) 203-0082",
-              "phone": "5712030082",
-              "address": "11935 Democracy Dr",
-              "city": "Reston",
-              "country": "United States",
-              "crossStreet": "btw Explorer & Library",
-              "postalCode": "20190",
-              "state": "VA"
-            }
-          },
-          {
-            "type": "Feature",
-            "geometry": {
-              "type": "Point",
-              "coordinates": [
-                -77.10853099823,
-                38.880100922392
-              ]
-            },
-            "properties": {
-              "phoneFormatted": "(703) 522-2016",
-              "phone": "7035222016",
-              "address": "4075 Wilson Blvd",
-              "city": "Arlington",
-              "country": "United States",
-              "crossStreet": "at N Randolph St.",
-              "postalCode": "22203",
-              "state": "VA"
-            }
-          },
-          {
-            "type": "Feature",
-            "geometry": {
-              "type": "Point",
-              "coordinates": [
-                -75.28784,
-                40.008008
-              ]
-            },
-            "properties": {
-              "phoneFormatted": "(610) 642-9400",
-              "phone": "6106429400",
-              "address": "68 Coulter Ave",
-              "city": "Ardmore",
-              "country": "United States",
-              "postalCode": "19003",
-              "state": "PA"
-            }
-          },
-          {
-            "type": "Feature",
-            "geometry": {
-              "type": "Point",
-              "coordinates": [
-                -75.20121216774,
-                39.954030175164
-              ]
-            },
-            "properties": {
-              "phoneFormatted": "(215) 386-1365",
-              "phone": "2153861365",
-              "address": "3925 Walnut St",
-              "city": "Philadelphia",
-              "country": "United States",
-              "postalCode": "19104",
-              "state": "PA"
-            }
-          },
-          {
-            "type": "Feature",
-            "geometry": {
-              "type": "Point",
-              "coordinates": [
-                -77.043959498405,
-                38.903883387232
-              ]
-            },
-            "properties": {
-              "phoneFormatted": "(202) 331-3355",
-              "phone": "2023313355",
-              "address": "1901 L St. NW",
-              "city": "Washington DC",
-              "country": "United States",
-              "crossStreet": "at 19th St",
-              "postalCode": "20036",
-              "state": "D.C."
-            }
-          }
+          
         ]
 };
 

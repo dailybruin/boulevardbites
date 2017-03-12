3c0712685edf4feb45ab9aaf710c85c5986bcd1a
Benson Han <bensonjaminhan@gmail.com>
2017-03-12 15:15:57 -0700

    blue

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiYmVuc29uaGFuIiwiYSI6ImNpdW4wc2V3NzAwZzAydG13eTB6bDdrdGMifQ.3qVecRB6mpgc1X-pURkDng';
 var map = L.mapbox.map('map-leaflet', 'mapbox.streets')
-  .setView([34.05048, -118.43791], 12);
+  .setView([34.04048, -118.43791], 12);
 var geojson = [
   {
     type: 'Feature',
@@ -11,7 +11,7 @@ var geojson = [
     properties: {
       title: '<h1 style="font-family: Pacifico, cursive; font-size: large;">Ramayani</h1>',
       description: '<h1 style="font-family: Lato; font-size: small">1777 Westwood Blvd Los Angeles, CA 90024</h1>',
-      'marker-color': '#f44336',
+      'marker-color': '#1565c0',
       'marker-size': 'medium',
       'marker-symbol': 'restaurant'
     }
@@ -25,7 +25,7 @@ var geojson = [
     properties: {
       title: '<h1 style="font-family: Pacifico, cursive; font-size: large;">The Butcher\'s Daughter</h1>',
       description: '<h1 style="font-family: Lato; font-size: small">1205 Abbot Kinney Blvd, Venice, CA 90291</h1>',
-      'marker-color': '#f44336',
+      'marker-color': '#1565c0',
       'marker-size': 'medium',
       'marker-symbol': 'restaurant'
     }
@@ -39,7 +39,7 @@ var geojson = [
     properties: {
       title: '<h1 style="font-family: Pacifico, cursive; font-size: large;">Hamasaku</h1>',
       description: '<h1 style="font-family: Lato; font-size: small">11043 California Route 2, Los Angeles, CA 90025</h1>',
-      'marker-color': '#f44336',
+      'marker-color': '#1565c0',
       'marker-size': 'medium',
       'marker-symbol': 'restaurant'
     }
@@ -53,7 +53,7 @@ var geojson = [
     properties: {
       title: '<h1 style="font-family: Pacifico, cursive; font-size: large;">OP Cafe</h1>',
       description: '<h1 style="font-family: Lato; font-size: small">3117 Ocean Park Blvd, Santa Monica, CA 90405</h1>',
-      'marker-color': '#f44336',
+      'marker-color': '#1565c0',
       'marker-size': 'medium',
       'marker-symbol': 'restaurant'
     }
@@ -67,7 +67,7 @@ var geojson = [
     properties: {
       title: '<h1 style="font-family: Pacifico, cursive; font-size: large;">Taste of Tehran | Tacos Tu Madre\'s</h1>',
       description: '<h1 style="font-family: Lato; font-size: small">1915 Westwood Blvd, Los Angeles, CA 90025 | 1945 Westwood Blvd, Los Angeles, CA 90025</h1>',
-      'marker-color': '#f44336',
+      'marker-color': '#1565c0',
       'marker-size': 'medium',
       'marker-symbol': 'restaurant'
     }
@@ -81,7 +81,7 @@ var geojson = [
     properties: {
       title: '<h1 style="font-family: Pacifico, cursive; font-size: large;">8oz Poke</h1>',
       description: '<h1 style="font-family: Lato; font-size: small">8871 Santa Monica Blvd West Hollywood, CA 90069</h1>',
-      'marker-color': '#f44336',
+      'marker-color': '#1565c0',
       'marker-size': 'medium',
       'marker-symbol': 'restaurant'
     }
@@ -95,7 +95,7 @@ var geojson = [
     properties: {
       title: '<h1 style="font-family: Pacifico, cursive; font-size: large;">Cake and Art</h1>',
       description: '<h1 style="font-family: Lato; font-size: small">8709 CA-2, West Hollywood, CA 90069</h1>',
-      'marker-color': '#f44336',
+      'marker-color': '#1565c0',
       'marker-size': 'medium',
       'marker-symbol': 'restaurant'
     }
@@ -109,7 +109,7 @@ var geojson = [
     properties: {
       title: '<h1 style="font-family: Pacifico, cursive; font-size: large;">Tsujita Annex | Tatsu Ramen | Daikokuya</h1>',
       description: '<h1 style="font-family: Lato; font-size: small">2050 | 2123 | 2208 Sawtelle Blvd, Los Angeles, CA 90025</h1>',
-      'marker-color': '#f44336',
+      'marker-color': '#1565c0',
       'marker-size': 'medium',
       'marker-symbol': 'restaurant'
     }
@@ -123,7 +123,7 @@ var geojson = [
     properties: {
       title: '<h1 style="font-family: Pacifico, cursive; font-size: large;">Fountain Coffee Room</h1>',
       description: '<h1 style="font-family: Lato; font-size: small">9641 Sunset Blvd, Beverly Hills, CA 90210</h1>',
-      'marker-color': '#f44336',
+      'marker-color': '#1565c0',
       'marker-size': 'medium',
       'marker-symbol': 'restaurant'
     }
@@ -137,7 +137,7 @@ var geojson = [
     properties: {
       title: '<h1 style="font-family: Pacifico, cursive; font-size: large;">Tuck Room Tavern</h1>',
       description: '<h1 style="font-family: Lato; font-size: small">10850 Wilshire Blvd, Los Angeles, CA 90024</h1>',
-      'marker-color': '#f44336',
+      'marker-color': '#1565c0',
       'marker-size': 'medium',
       'marker-symbol': 'restaurant'
     }
